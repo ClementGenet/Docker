f78610a0cdcf919d879f7798f07d42e01ea9167e
ClementGenet <86838747+ClementGenet@users.noreply.github.com>
2021-07-02 15:40:22 +0200

    Update start.sh

--- web/start.sh
+++ web/start.sh
@@ -5,8 +5,8 @@ if [ -z "$(ls -A /var/www/html/wordpress)" ]; then
    curl -s -u $HTTP_SRC_LOGIN:$HTTP_SRC_PWD $HTTP_SRC_WWW > /dev/null
    curl -s -u $HTTP_SRC_LOGIN:$HTTP_SRC_PWD $HTTP_SRC_DB > /dev/null
    unzip /var/www/src/*.zip -d /var/www/html/
-   sed -i 's/10.76.1.72/'$HOST_IP'/g' /var/www/src/*.sql
-   sed -i 's/10.176.128.142/'$HOST_IP'/g' /var/www/src/*.sql
+   sed -i 's/192.168.1.10/'$HOST_IP'/g' /var/www/src/*.sql
+   sed -i 's/192.168.1.25/'$HOST_IP'/g' /var/www/src/*.sql
    #rm /var/www/src/*
    sed -i 's/localhost/db/g' /var/www/html/*/wp-config.php
    rm /var/www/html/*/.htaccess
